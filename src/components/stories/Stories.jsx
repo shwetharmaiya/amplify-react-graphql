83a8afdb332fa3c221c25e8fd71acede48f0f72e
shwetharmaiya <shwetharmaiya@gmail.com>
2023-07-07 23:15:34 +0530

    Amplify code changes

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -6,33 +6,33 @@ const Stories = () => {
  
   const currentUser = "Krishna";
 
-  const { isLoading, error, data } = useQuery(["stories"], () =>
-    makeRequest.get("/stories").then((res) => {
-      return res.data;
-    })
-  );
+  // const { isLoading, error, data } = useQuery(["stories"], () =>
+  //   makeRequest.get("/stories").then((res) => {
+  //     return res.data;
+  //   })
+  // );
 
-  //TODO Add story using react-query mutations and use upload function.
+  // //TODO Add story using react-query mutations and use upload function.
 
-  return (
-    <div className="stories">
-      <div className="story">
-        <img src={"/upload/" + currentUser.profilePic} alt="" />
-        <span>{currentUser.name}</span>
-        <button>+</button>
-      </div>
-      {error
-        ? "Something went wrong"
-        : isLoading
-        ? "loading"
-        : data.map((story) => (
-            <div className="story" key={story.id}>
-              <img src={story.img} alt="" />
-              <span>{story.name}</span>
-            </div>
-          ))}
-    </div>
-  );
+  // return (
+  //   <div className="stories">
+  //     <div className="story">
+  //       <img src={"/upload/" + currentUser.profilePic} alt="" />
+  //       <span>{currentUser.name}</span>
+  //       <button>+</button>
+  //     </div>
+  //     {error
+  //       ? "Something went wrong"
+  //       : isLoading
+  //       ? "loading"
+  //       : data.map((story) => (
+  //           <div className="story" key={story.id}>
+  //             <img src={story.img} alt="" />
+  //             <span>{story.name}</span>
+  //           </div>
+  //         ))}
+  //   </div>
+  // );
 };
 
 export default Stories;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import "./home.scss"
-import Stories from "../../components/stories/Stories"
+// import Stories from "../../components/stories/Stories"
 import Posts from "../../components/posts/Posts"
-import Share from "../../components/share/Share"
+// import Share from "../../components/share/Share"
 
 const Home = () => {
   return (
